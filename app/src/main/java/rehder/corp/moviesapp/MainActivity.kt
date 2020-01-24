092a0b167f2287e97a0209bfd7cc17e8395bbe4e
Filipe Rehder <filipe.rehder@gmail.com>
2020-01-23 22:51:43 -0300

    first commit

---
+++ app/src/main/java/rehder/corp/moviesapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package rehder.corp.moviesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
